8da9db3014a821c531be4c4b8b07cb962d11e4e3
Danylo Kryzhnii <danilonok1906@gmail.com>
2025-04-05 01:23:48 +0200

    Added prompt for task generation

--- prompts/prompts.py
+++ prompts/prompts.py
@@ -1,8 +1,7 @@
 from llama_index.core.prompts import PromptTemplate
 
-DEFAULT_COMPLETION_PROMPT_TMPL = (
-    "You are an Windows PC Assistant. Answer the query.\n"
-    "Query: {query_str}\n"
-    "Answer: "
+DEFAULT_TASK_GENERATION_TMPL = (
+    "You must create {num_tasks} comlex tasks that can be performed within Windows 11 operating system\n"
+    "It should be possible to execute them using Powershell. They must be complex enough to execute them in at least two steps."
+    "The task must be as if the real PC user would ask for it.\n"
 )
-default_completion_prompt = PromptTemplate(DEFAULT_COMPLETION_PROMPT_TMPL)
