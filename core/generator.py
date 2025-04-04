06c91404ecf0088985839d07971819577c2ef6d7
Danylo Kryzhnii <danilonok1906@gmail.com>
2025-04-05 01:21:08 +0200

    Basic text generation with Pydantic parser

---
+++ core/generator.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from configs.llm_config import Settings
+from typing import Type
+from llama_index.core.program import LLMTextCompletionProgram
+from llama_index.core.output_parsers import PydanticOutputParser
+class BaseGenerator:
+    """Base generator wrapper class to generate text given prompt"""
+    def __init__(self, prompt: str, model: str, output_model: Type[BaseModel]):
+        self.prompt_str = prompt
+        if model=='ollama':
+            self.llm = Settings.llm
+        else:
+            self.llm = None
+        assert self.llm, "LLM must be defined"
+        self.output_model = output_model
+    def generate(query: str):
+        parser = LLMTextCompletionProgram.from_defaults(
+            output_parser=PydanticOutputParser(self.output_model),
+            prompt_template_str=self.prompt_str,
+            llm=self.llm
+        ) 
+        response = parser(query=query)   
+        if len(responce.tasks) != num_tasks:
+            print(f"WARNING: Expected {num_tasks} tasks, but got {len(responce.tasks)}")
+        return response.tasks
+    def agenerate():
+        pass
