a45ae9c26af204a8c9da3ffb2634c1bb5cbb3646
danilonok <danilonok@DANILONOKGM>
2025-05-24 19:36:27 +0200

    Basic file operations are added to file_agent, closes #2, closes #3, closes #4, closes #7

--- workflows/file_agent.py
+++ workflows/file_agent.py
@@ -3,7 +3,8 @@ from llama_index.core.workflow import Context
 from multipart import file_path
 
 import shutil
-
+import os
+import send2trash
 
 async def copy_file(ctx: Context, origin: str, destination: str) -> str:
     """Useful for copying file from origin location in PC to destination location. Your input must contain full paths of origin and destination of a copy operation. Both must contain name of file with an extension at the end
@@ -22,24 +23,75 @@ async def copy_file(ctx: Context, origin: str, destination: str) -> str:
 
 
 
-async def create_file(ctx: Context, name: str, file_path:str) -> str:
-    """Useful for creating a file using the path. Your input must contain name of a file with extension and full path of a place, where this file should be created."""
+async def create_file(ctx: Context, file_path:str) -> str:
+    """Useful for creating an empty file using the path. Your input must contain and full path of a future file containing name of a file with extension."""
     # Create file
-    raise NotImplementedError()
-    return f'File {name} was created in {file_path} successfully.'
+    try:
+        with open(file_path, 'x') as file:
+            pass
+        return f'File was created in {file_path} successfully.'
+    except FileExistsError:
+        return "File already exists."
+    except Exception as e:
+        return f"An error occurred: {e}"
+
 
 async def delete_file(ctx: Context, file_path:str) -> str:
     """Useful for deleting a file using the path. Your input must contain a full path of a file."""
     # Delete file
-    raise NotImplementedError()
-    return f'File {file_path} was deleted successfully.'
+    try:
+        send2trash.send2trash(file_path.replace('/', '\\'))
+        return f'File {file_path} was deleted successfully.'
+    except FileNotFoundError:
+        return "File not found."
+    except PermissionError:
+        return "Permission denied."
+    except Exception as e:
+        return f"An error occurred: {e}"
+
 
 async def write_file(ctx: Context, content: str, file_path:str) -> str:
     """Useful for writing to an existing file. Your input must contain a full path of a file and a content as a string."""
     # Write to file
-    raise NotImplementedError()
-    return f'Content {content[:50]} was written successfully to{file_path}'
+    try:
+        with open(file_path, 'w') as file:
+            file.write(content)
+        return f'Content was written successfully to {file_path}'
+    except PermissionError:
+        return "Permission denied."
+    except Exception as e:
+        return f"An error occurred: {e}"
 
+
+async def read_file(ctx: Context, file_path:str) -> str:
+    """Useful for reading content of a file using its path. Your input must contain a full path of a file."""
+    # Read file
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            return f'Contents: \n{content}'
+    except FileNotFoundError:
+        return "File not found."
+    except PermissionError:
+        return "Permission denied."
+
+
+async def dir_path(ctx: Context, path:str) -> str:
+    """Useful for checking structure of a file system given path. Your input must contain a path."""
+    # Dir folders
+    def print_tree(startpath, max_depth=3, indent="  "):
+        content = ''
+        for root, dirs, files in os.walk(startpath):
+            level = root.replace(startpath, '').count(os.sep)
+            if level >= max_depth:
+                continue
+            indent_str = indent * level
+            content += f"{indent_str}{os.path.basename(root)}/\n"
+            subindent = indent * (level + 1)
+            for f in files:
+                content += f"{subindent}{f}\n"
+        return content
+    return f'Structure of a path {path}:\n{print_tree(path)}'
 
 file_agent = FunctionAgent(
     name='FileAgent',
@@ -48,7 +100,7 @@ file_agent = FunctionAgent(
         "You are FileAgent that can perform various actions with PC's file system."
         "Given a request, you should fulfil it and provide concise response as a report of your work."
     ),
-    tools=[copy_file],
+    tools=[copy_file, create_file, write_file, delete_file, read_file, dir_path],
     can_handoff_to=['AgentOrchestrator']
 )
 
